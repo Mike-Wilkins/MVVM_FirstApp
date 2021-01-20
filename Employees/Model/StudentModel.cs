28abde56ede2c32238d5a79b93d31e40a0cf24f3
Mike <michael-wilkins@sky.com>
2021-01-20 17:19:01 +0000

    Add project files.

---
+++ Employees/Model/StudentModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Model
+{
+    public class StudentModel
+    {
+    }
+
+    public class Student : INotifyPropertyChanged
+    {
+        private string firstname;
+        private string surname;
+
+        public string Firstname
+        {
+            get { 
+                return firstname; }
+            set {
+                if(firstname != value)
+                {
+                    firstname = value;
+                    RaisePropertyChanged("Firstname");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                if (surname != value)
+                {
+                    surname = value;
+                    RaisePropertyChanged("Surname");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return firstname + " " + surname;
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string property)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ Employees/ViewModel/StudentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Employees.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+            students.Add(new Student { Firstname = "Tom", Surname = "Lewis" });
+            students.Add(new Student { Firstname = "Sarah", Surname = "Williams" });
+            students.Add(new Student { Firstname = "James", Surname = "Taylor" });
+
+            Students = students;
+        }
+    }
+}
